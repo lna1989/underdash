7f56ffcfda614957c520481f4d2e94a8b1e7b8e0
lna1989 <nikita@loshchenin.ru>
2020-05-20 21:35:41 +0300

    add chunk description

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 class Underdash {
+    /**
+     * Creates an array of elements split into groups the length of size. If array can't be split evenly, the final chunk will be the remaining elements.
+     * @param array
+     * @param counter
+     * @returns array
+     */
     static chunk(array = [], counter = 1) {
         return array.reduce((previousValue, currentValue, index) => {
             if (index % counter === 0) {
                 return [...previousValue, [currentValue]];
             }
-             return [...previousValue.slice(0, -1), [...previousValue.slice(-1)[0], currentValue]];
+            return [...previousValue.slice(0, -1), [...previousValue.slice(-1)[0], currentValue]];
         }, []);
     }
 
@@ -13,3 +19,4 @@ class Underdash {
 }
 
 console.log(Underdash.chunk(['abs', 'b', 'c', 'd', 'e', 'f'], 2));
+console.log(Underdash.chunk([], 2));
